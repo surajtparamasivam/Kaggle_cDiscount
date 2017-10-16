b2695fc4830d1ef50a1509fb9e87b7e3748b7ceb
Suraj Paramasivam <sujisuraj@gmail.com>
2017-10-16 13:17:17 -0400

    Working version cDiscount1

---
+++ Scrap.py
@@ -0,0 +1,17 @@
+import io
+from skimage.data import imread
+from pymongo import MongoClient
+import time
+import bson
+import json
+
+client = MongoClient() #Makes it "good enough" for our multi-threaded use case.
+train = client.cDiscount['train']
+test = client.cDiscount['test']
+
+data=train.find_one({'_id':6000})
+img = imread(io.BytesIO(data['imgs'][0]['picture']))
+label = data['category_id']
+print(label)
+print(img)
+
